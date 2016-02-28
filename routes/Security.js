1f76641f3efe8691dd2671b39d3faafad3a88949
Chorn Sokun <chornsokun@gmail.com>
2016-02-28 12:13:33 +1100

    testing env. var

--- routes/Security.js
+++ routes/Security.js
@@ -2,8 +2,7 @@
 // https://github.com/auth0/express-jwt
 var express = require('express'), 
     jwt = require('express-jwt'),
-    cfenv = require('cfenv'),
-    appEnv = cfenv.getAppEnv(),
+    cfenv = require('../cfenv-wrapper'),
     router = express.Router();
 
 // router.get('/auth', jwt({secret: 'shhhhhhared-secret'}), function(req, res) {
@@ -12,6 +11,7 @@ var express = require('express'),
 //   });
 
 router.get('/secret', function (req, res, next) {
+    var appEnv = cfenv.getAppEnv();
     res.send({secret: appEnv.getEnvVar('secret') });
     next();
 });
